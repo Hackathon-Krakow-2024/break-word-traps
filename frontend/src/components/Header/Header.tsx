c20dce02c1adf70fa879dba00009d3dfe910caaa
Michal <figielek.michal@gmail.com>
2024-09-28 23:15:06 +0200

    Some colors improvements

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className='bg-slate-400 text-white shadow-lg'>
+    <header className='bg-sky-600 text-white shadow-lg'>
       <div className='container mx-auto flex items-center justify-between px-6 py-4'>
         <div className='text-2xl font-bold'>
           <a href='/' className='hover:text-gray-300'>
