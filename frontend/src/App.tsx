8cae252fcf33f47032687d80cc1ce91d90703fa3
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2024-09-29 06:47:56 +0200

    remove contact

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import { Header } from './components/Header/Header'
 import { Home } from './pages/Home/Home'
-import { Contact } from './pages/Contact/Contact'
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
