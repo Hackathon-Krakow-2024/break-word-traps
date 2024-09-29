d65634745dbb983e7f61d231f83d67de3284e530
Michal <figielek.michal@gmail.com>
2024-09-29 02:22:38 +0200

    Header styles improvements

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
   return (
-    <header className='bg-sky-600 text-white shadow-lg'>
+    <header className='bg-gradient-to-r from-sky-500 to-blue-600 text-white shadow-lg'>
       <div className='container mx-auto flex items-center justify-between px-6 py-4'>
-        <div className='text-2xl font-bold'>
-          <a href='/' className='hover:text-gray-300'>
+        <div className='text-3xl font-extrabold'>
+          <a href='/' className='transition-colors duration-300 hover:text-gray-300'>
             Words Smoothie
           </a>
         </div>
